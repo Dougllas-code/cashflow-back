e2f9ce23b78d3079f79b68c82775fcc3733f6de0
Dougllas Souza <dougllas58@outlook.com>
2025-03-19 08:38:37 -0300

    feat: created dbcontext

---
+++ src/CashFlow.Infra/DataAccess/CashFlowDbContext.cs
@@ -0,0 +1,18 @@
+﻿using CashFlow.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace CashFlow.Infra.DataAccess
+{
+    public class CashFlowDbContext: DbContext
+    {
+        public DbSet<Expense> Expenses { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            var connectionString = "Server=localhost;Database=cashflowdb;Uid={yourUserName};Pwd={yourPassword};";
+            var serverVersion = new MySqlServerVersion(new Version(9, 2, 0));
+
+            optionsBuilder.UseMySql(connectionString, serverVersion);
+        }
+    }
+}
