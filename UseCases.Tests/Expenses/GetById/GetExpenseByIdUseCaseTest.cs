954bcd6bbe72b69827f154727c67c31efee3ba14
Dougllas Souza <dougllas58@outlook.com>
2025-08-14 09:05:07 -0300

    implemented tests

---
+++ UseCases.Tests/Expenses/GetById/GetExpenseByIdUseCaseTest.cs
@@ -0,0 +1,62 @@
+﻿using CashFlow.Application.UseCases.Expenses.GetById;
+using CashFlow.Domain.Entities;
+using CashFlow.Exception;
+using CashFlow.Exception.BaseExceptions;
+using CommonTestUtilities.Entities.User;
+using CommonTestUtilities.LoggerUser;
+using CommonTestUtilities.Mapper;
+using CommonTestUtilities.Repositories;
+
+namespace UseCases.Tests.Expenses.GetById
+{
+    public class GetExpenseByIdUseCaseTest
+    {
+        [Fact]
+        public async Task Success()
+        {
+            // Arrange
+            var user = UserBuilder.Build();
+            var expense = ExpenseBuilder.Build(user);
+
+            var useCase = CreateUseCase(user, expense);
+
+            //Act
+            var result = await useCase.Execute(expense.Id);
+
+            //Assert
+            Assert.NotNull(result);
+            Assert.Equal(expense.Id, result.Id);
+            Assert.Equal(expense.Title, result.Title);
+            Assert.Equal(expense.Description, result.Description);
+            Assert.Equal(expense.Date, result.Date);
+            Assert.Equal(expense.Amount, result.Amount);
+            Assert.Equal(expense.PaymentType, (CashFlow.Domain.Enums.PaymentType)result.PaymentType);
+        }
+
+        [Fact]
+        public async Task Error_Expense_Not_Found()
+        {
+            // Arrange
+            var user = UserBuilder.Build();
+
+            var useCase = CreateUseCase(user);
+
+            //Act
+            var exception = await Assert.ThrowsAsync<NotFoundException>(() => useCase.Execute(id: 10000));
+
+            //Assert
+            Assert.NotNull(exception);
+            Assert.Contains(ResourceErrorMessages.EXPENSE_NOT_FOUND, exception.GetErrors());
+            Assert.Single(exception.GetErrors());
+        }
+
+        private GetByIdUseCase CreateUseCase(User user, Expense? expense = null)
+        {
+            var repository = new ExpensesReadOnlyRepositoryBuilder().GetById(user, expense).Build();
+            var mapper = MapperBuilder.Build();
+            var loggedUser = LoggedUserBuilder.Build(user);
+
+            return new GetByIdUseCase(repository, mapper, loggedUser);
+        }
+    }
+}
---
+++ WebApi.Tests/Expenses/GetById/GetExpenseByIdTest.cs
@@ -0,0 +1,42 @@
+﻿using CashFlow.Domain.Enums;
+using System.Text.Json;
+
+namespace WebApi.Tests.Expenses.GetById
+{
+    public class GetExpenseByIdTest: CashFlowClassFixture
+    {
+        private const string METHOD = "api/Expenses";
+
+        private readonly string _token;
+        private readonly long _expenseId;
+
+        public GetExpenseByIdTest(CustomWebApplicationFactory webApplicationFactory):base(webApplicationFactory)
+        {
+            _token = webApplicationFactory.GetToken();
+            _expenseId = webApplicationFactory.GetExpenseId();
+        }
+
+        [Fact]
+        public async Task Success()
+        {
+            // Act
+            var result = await DoGet($"{METHOD}/{_expenseId}", _token);
+
+            // Assert
+            Assert.Equal(System.Net.HttpStatusCode.OK, result.StatusCode);
+
+            var responseBody = await result.Content.ReadAsStreamAsync();
+            var responseData = await JsonDocument.ParseAsync(responseBody);
+
+            Assert.NotNull(responseData);
+            Assert.Equal(_expenseId, responseData.RootElement.GetProperty("id").GetInt64());
+            Assert.NotNull(responseData.RootElement.GetProperty("title").GetString());
+            Assert.NotEmpty(responseData.RootElement.GetProperty("title").GetString()!);
+            Assert.True(responseData.RootElement.GetProperty("date").GetDateTime() < DateTime.Today);
+            Assert.True(responseData.RootElement.GetProperty("amount").GetDecimal() > 0);
+
+            var paymentTypeValue = responseData.RootElement.GetProperty("paymentType").GetInt32();
+            Assert.True(Enum.IsDefined(typeof(PaymentType), paymentTypeValue));
+        }
+    }
+}
